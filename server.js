3d8a13df090401772386e8c22ed292094d31b666
Adson <adson23_@hotmail.com>
2024-07-24 09:43:30 -0300

    Add Query Params filter

--- server.js
+++ server.js
@@ -22,9 +22,49 @@ app.post('/users', async (req, res) => {
 
 app.get('/users', async (req, res) => {
 
-    const users = await prisma.user.findMany()
+    let users = []
+
+    if (req.query) {
+
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age,
+                email: req.query.email
+            }
+        })
+
+    } else {
+
+        await prisma.user.findMany()
+    }
 
     res.status(200).json(users)
+})
+
+app.put('/users/:id', async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: 'User has been successfully deleted!' })
 })
 
 console.log("Server is running!")
